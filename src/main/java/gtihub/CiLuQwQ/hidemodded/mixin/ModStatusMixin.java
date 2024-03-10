afddfd592b8c759dfae5552f8067a270e7c5abcc
CiLuQwQ <a@>
2024-03-10 13:43:40 +0800

    DoneTaskOne

---
+++ src/main/java/gtihub/CiLuQwQ/hidemodded/mixin/ModStatusMixin.java
@@ -0,0 +1,16 @@
+package gtihub.CiLuQwQ.hidemodded.mixin;
+
+import net.minecraft.util.ModStatus;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+@Mixin(ModStatus.class)
+public class ModStatusMixin {
+	/**
+	 * @author CiLuQwQ
+	 * @reason
+	 */
+	@Overwrite
+	public boolean isModded() {
+		return false;
+	}
+}
